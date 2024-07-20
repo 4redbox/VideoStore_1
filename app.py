5051c5fb202e87317c7318e1db9d4b4dacb70315
cc-user <raja4redbox@gmail.com>
2024-07-21 04:21:38 +0530

    add jsonify

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, jsonify
 #import mysql.connector
 import boto3
 from botocore.exceptions import ClientError
